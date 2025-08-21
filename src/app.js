5a101595286a5ef5e955a14fd962587596babad9
codemencer-rahul <rahulrajak10169@gmail.com>
2025-08-21 20:15:09 +0530

    add user and video model

--- src/app.js
+++ src/app.js
@@ -7,11 +7,11 @@ const app = express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
-}))
+})) // security
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({limit: "16kb"})) // api se jo bhi api json data bheja jayega, use server readable format me bna dega(js object).
+app.use(express.urlencoded({extended: true, limit: "16kb"})) // form ka deta jo ki url-encoding hoke aata hai, use server readable bna deta hai by decoding.
+app.use(express.static("public")) // jb bhi browser static files ko khoje to server tum use public folder me khojo
+app.use(cookieParser()) // remembers old request
 
 export { app }
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -21,3 +21,10 @@ class ApiError extends Error {
 }
 
 export { ApiError }
+
+
+/* Error ka msg structure:
+stage 1 => success,msg,statusCode,data,
+stage 2 => error[],stack
+
+Note:- error me this.data hamesha null hi hoga, kyuki koi data aaya hi nhi. also this.success = false*/
